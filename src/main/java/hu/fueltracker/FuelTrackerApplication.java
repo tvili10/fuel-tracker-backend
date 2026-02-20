7e21d63d33d669a8574371951c10e5aa59d1c7ed
tvili10 <80966514+tvili10@users.noreply.github.com>
2026-02-20 12:11:02 +0100

    initial commit

---
+++ src/main/java/hu/fueltracker/FuelTrackerApplication.java
@@ -0,0 +1,13 @@
+package hu.fueltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuelTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/fueltracker/FuelTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.fueltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
